3ecccaadd41dc07dc41bc865d6494edbc6239bc3
ranjan <ranjan@ranjan.com>
2022-01-22 13:42:27 -0800

    testing husky

--- src/components/accordionunit.tsx
+++ src/components/accordionunit.tsx
@@ -1,7 +1,9 @@
 import { Box, Heading } from "@chakra-ui/layout";
 import {
   AccordionButton,
-  AccordionIcon, AccordionItem, AccordionPanel
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
 } from "@chakra-ui/react";
 import React from "react";
 type props = {
@@ -10,13 +12,14 @@ type props = {
 };
 const AccordionUnit: React.FC<props> = ({ title, children }) => {
   return (
-    <AccordionItem border="none" flex="1" bg="white">
+    <AccordionItem border="none" flex="1" bg="white  "    >
       <AccordionButton _expanded={{ bg: "gray.200" }}>
         <Box flex="1" textAlign="left">
           <Heading fontWeight="light" as="h2" size="md" isTruncated>
             {title}
           </Heading>
         </Box>
+
         <AccordionIcon />
       </AccordionButton>
       <AccordionPanel py={8} px={0}>
